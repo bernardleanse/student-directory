6bb551a2bb71ce71becd89ede9c816d9e22cfc99
Bernard Leanse <bernardleanse96@gmail.com>
2021-09-13 13:55:30 +0100

    added-puts-totalstudents

--- directory.rb
+++ directory.rb
@@ -1,3 +1,5 @@
+student_count = 11
+
 puts "The students of Villains Accademy"
 puts "------------------------"
 puts "Dr Hannibal Lecter"
@@ -11,3 +13,5 @@ puts "Freddy Krueger"
 puts "The Joker"
 puts "Joffrey Baratheon"
 puts "Norman Bates"
+# Displaying the total number of students
+puts "Overall we have #{student_count} students at the accademy"
