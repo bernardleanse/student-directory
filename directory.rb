88be11dcea91a14243ef5dfbe7b92fec4d6dc12f
Bernard Leanse <bernardleanse96@gmail.com>
2021-09-14 12:08:27 +0100

    created hashes

--- directory.rb
+++ directory.rb
@@ -1,15 +1,15 @@
 student_list = [
-  "Dr Hannibal Lecter",
-  "Darth Vader",
-  "Nurse Ratched",
-  "Micheal Corelone",
-  "Alex DeLarge",
-  "The Wicked Witch of the West",
-  "Terminator",
-  "Freddy Krueger",
-  "The Joker",
-  "Joffrey Baratheon",
-  "Norman Bates" 
+  {:name => "Dr Hannibal Lecter", :cohort => :september },
+  {:name => "Darth Vader", :cohort => :september },
+  {:name => "Nurse Ratched", :cohort => :september },
+  {:name => "Micheal Corelone", :cohort => :september },
+  {:name => "Alex DeLarge", :cohort => :september },
+  {:name => "The Wicked Witch of the West", :cohort => :september },
+  {:name => "Terminator", :cohort => :september },
+  {:name => "Freddy Krueger", :cohort => :september},
+  {:name => "The Joker", :cohort => :september },
+  {:name => "Joffrey Baratheon", :cohort => :september },
+  {:name => "Norman Bates", :cohort => :september } 
 ]
 
 # prints the header
@@ -20,7 +20,7 @@ end
 
 # gives every indivudal name given to each students string object in list
 def print_each_name(name_list)
-  name_list.each { |student| puts student}
+  name_list.each { |student| puts "#{student[:name]} (#{student[:cohort]} cohort)"}
 end 
 
 # Displaying the total number of students
@@ -33,4 +33,8 @@ end
 print_header()
 print_each_name(student_list)
 print_footer(student_list)
+
+
+
+
 
