166387377044b6881bb9270cc2314f6e4667c347
Bernard Leanse <bernardleanse96@gmail.com>
2021-09-15 14:26:46 +0100

    interactive menu

--- directory.rb
+++ directory.rb
@@ -3,12 +3,12 @@ student_list = [
   {:name => "Darth Vader", :cohort => :september, height: "170cm", birth_country: "" },
   {:name => "Nurse Ratched", :cohort => :september, height: "170cm", birth_country: "" },
   {:name => "Micheal Corelone", :cohort => :september, height: "170cm", birth_country: "" },
-  {:name => "Alex DeLarge", :cohort => :september, height: "170cm", birth_country: "" },
+  {:name => "Alex DeLarge", :cohort => :november, height: "170cm", birth_country: "" },
   {:name => "The Wicked Witch of the West", :cohort => :september, height: "170cm", birth_country: "" },
   {:name => "Terminator", :cohort => :september, height: "170cm", birth_country: "" },
-  {:name => "Freddy Krueger", :cohort => :september, height: "170cm", birth_country: ""},
+  {:name => "Freddy Krueger", :cohort => :october, height: "170cm", birth_country: ""},
   {:name => "The Joker", :cohort => :september, height: "170cm", birth_country: "" },
-  {:name => "Joffrey Baratheon", :cohort => :september, height: "170cm", birth_country: "" },
+  {:name => "Joffrey Baratheon", :cohort => :october, height: "170cm", birth_country: "" },
   {:name => "Norman Bates", :cohort => :september, height: "170cm", birth_country: "" } 
 ]
 
@@ -17,6 +17,14 @@ class String
     months = "january february march april may june july august september october november december".split
     return true if months.include? self
     return false
+  end
+end
+
+class Array
+  def order_by_month
+    months = "january february march april may june july august september october november december".split
+    months.map! { |month| month.to_sym }
+    return months.select{ |month| self.include? month }
   end
 end
 
@@ -32,7 +40,18 @@ end
 #   name_list.each.with_index { |student, i| puts "#{i + 1}. #{student[:name]} (#{student[:cohort]} cohort)"}
 # end 
 
+def creates_ordered_list(list)
+  ordered_list = []
+  cohorts = list.map { |student| student[:cohort] }.uniq
+  cohorts = cohorts.order_by_month
+  cohorts.each do |month|
+    list.each { |student| ordered_list << student if student[:cohort] == month }
+  end
+  return ordered_list
+end
+
 def print_each_name(name_list)
+  name_list = creates_ordered_list(name_list)
   n = 0
    
   while true
@@ -87,7 +106,8 @@ def create_student_list
     student_cohort = "september" if student_cohort.empty?
 
     students << {name: student_name, cohort: student_cohort.to_sym }
-    puts "We currently have #{students.length} students"
+    puts "We currently have #{students.length} student" if students.length == 1
+    puts "We currently have #{students.length} students" unless students.length == 1
     puts "Enter stop when you've finished"
     
   end
@@ -98,12 +118,45 @@ end
 def append_student_list(list)
   list.push(create_student_list).flatten!
 end
+
+def interactive_menu
+  student_list = Array.new
+
+  loop do
+  puts "Select and option: "
+  puts "1. Input Students"
+  puts "2. List all Students"
+  puts "9. Exit"
+    
+    user_selection = gets.chomp
+
+
+    case user_selection
+    when "1"
+      student_list = create_student_list
+    when "2"
+      print_header()
+      print_each_name(student_list)
+      print_footer(student_list)
+    when "9"
+      break
+    else
+      "Error, Try Again."
+    end
+
+  end
+
+end
+
 # method calls
 
-student_list = create_student_list
-print_header()
-print_each_name(student_list)
-print_footer(student_list)
+interactive_menu()
+
+# student_list = create_student_list
+# print_header()
+# print_each_name(student_list)
+# print_footer(student_list)
+
 
 
 
